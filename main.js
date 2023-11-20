9bccca148d29c9d5cf04df28ba26caef279ee48a
daeun55 <ghenmm@naver.com>
2023-11-20 23:01:42 +0900

    수정

--- main.js
+++ main.js
@@ -1,7 +1,49 @@
 "use strict";
 
+let target = document.querySelector("#dynamic");
+let stringArr = ["Developer", "DAEUN KIM", "김다은"];
+
+let selectString = stringArr[Math.floor(Math.random()*stringArr.length)];
+let selectStringArr = selectString.split("");
 
 
+function randomString(){
+    let stringArr = ["Developer", "DAEUN KIM", "김다은"];
+    let selectString = stringArr[Math.floor(Math.random()*stringArr.length)];
+    let selectStringArr = selectString.split("");
+
+    return selectStringArr;
+}
+
+
+function resetTyping(){
+    target.textContent="";
+    dynamic(randomString());
+}
+function dynamic(randomArr){
+    console.log(randomArr);
+    if(randomArr.length>0){
+        target.textContent+=randomArr.shift();
+        setTimeout(function(){
+            dynamic(randomArr);
+        },80);
+    }else{
+        setTimeout(resetTyping, 3000);
+    }
+}
+
+dynamic (randomString());
+console.log(selectString);
+console.log(selectStringArr);
+
+
+//커서 깜빡임 
+function blink(){
+    target.classList.toggle("active");
+}
+setInterval(blink, 500);
+
+//메뉴 섹션 셀렉터 함수
 const skBtn = document.querySelector(".sk");
 skBtn.addEventListener("click", () => {
   console.log("button click");
